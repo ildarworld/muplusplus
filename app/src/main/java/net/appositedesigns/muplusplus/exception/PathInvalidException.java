35a0704a844f8775c05477d335d2cd7ed1c619f2
Ильдар <ildarworld@ildarworld.local>
2017-03-15 00:03:22 +0300

    Add exceptions package

---
+++ app/src/main/java/net/appositedesigns/muplusplus/exception/PathInvalidException.java
@@ -0,0 +1,23 @@
+package net.appositedesigns.muplusplus.exception;
+
+public class PathInvalidException extends Exception {
+
+	private static final long serialVersionUID = -4046926680600982016L;
+	
+	private String location;
+
+	public PathInvalidException(String location) {
+		super();
+		this.location = location;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	
+}
