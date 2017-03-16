911ba476594057cb9410b64a68dc543ab17d09d7
ildarworld <ildarworld@gmail.com>
2017-03-16 23:38:17 +0300

    Add some files

--- app/src/main/java/net/appositedesigns/muplusplus/MuPlusPlusApp.java
+++ app/src/main/java/net/appositedesigns/muplusplus/MuPlusPlusApp.java
@@ -5,18 +5,18 @@ import android.content.Intent;
 
 public class MuPlusPlusApp extends Application {
 
-	public static final int THEME_BLACK = R.style.Theme_FileExplorer;
-	public static final int THEME_WHITE = R.style.Theme_FileExplorer_Light;
+	public static final int THEME_BLACK = R.style.Theme_Muplusplus;
+	public static final int THEME_WHITE = R.style.Theme_Muplusplus_Light;
 	public static final int THEME_WHITE_BLACK = android.R.style.Theme_Holo_Light_DarkActionBar;
 	
 	
-	public static final String ACTION_OPEN_BOOKMARK = "net.appositedesigns.fileexplorer.action.OPEN_BOOKMARKS";
-	public static final String ACTION_OPEN_FOLDER = "net.appositedesigns.fileexplorer.action.OPEN_FOLDER";
-	public static final String EXTRA_IS_PICKER = "net.appositedesigns.fileexplorer.extra.IS_PICKER";
+	public static final String ACTION_OPEN_BOOKMARK = "net.appositedesigns.MuPlusPlusApp.action.OPEN_BOOKMARKS";
+	public static final String ACTION_OPEN_FOLDER = "net.appositedesigns.MuPlusPlusApp.action.OPEN_FOLDER";
+	public static final String EXTRA_IS_PICKER = "net.appositedesigns.MuPlusPlusApp.extra.IS_PICKER";
 	public static final int REQ_PICK_FILE = 10;
 	public static final int REQ_PICK_BOOKMARK = 11;
-	public static final String EXTRA_SELECTED_BOOKMARK = "net.appositedesigns.fileexplorer.extra.SELECTED_BOOKMARK";
-	public static final String EXTRA_FOLDER = "net.appositedesigns.fileexplorer.extra.FOLDER";
+	public static final String EXTRA_SELECTED_BOOKMARK = "net.appositedesigns.MuPlusPlusApp.extra.SELECTED_BOOKMARK";
+	public static final String EXTRA_FOLDER = "net.appositedesigns.MuPlusPlusApp.extra.FOLDER";
 	
 	private Intent fileAttachIntent;
 
